13571961a5a2e70c7911ec0f7505255b1df7c794
julianbopp <julian.bopp@tutanota.com>
2022-08-11 13:29:15 +0200

    add basic functionality of calculator

--- external.js
+++ external.js
@@ -10,7 +10,7 @@ function multiply(a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 }
 
@@ -29,10 +29,59 @@ function getDisplayValue() {
 }
 
 
+function setup() {
+    const display = document.querySelector(".display");
+    var a = "";
+    var b = "";
+    var op = null;
+    var comma = false;
 
-let digits = document.querySelectorAll(".digit");
-console.log(digits);
+    const digits = document.querySelectorAll(".digit");
+    digits.forEach(digit => digit.addEventListener("click", function() {
+        if (digit.id === "comma" && comma) {
+            return;
+        }
+        if (digit.id === "comma" && !comma) {
+            comma = true;
+        }
+        show(digit.textContent);
+        if (!op) {
+            a = a + digit.textContent;
+        } else {
+            b = b + digit.textContent;
+        }
+    }));
 
-digits.forEach(digit => digit.addEventListener("click", function() {
-    show(digit.textContent);
-}));
+    const operators = document.querySelectorAll(".operator");
+    operators.forEach(operator => operator.addEventListener("click", function() {
+        if(!op) {
+            show(operator.textContent);
+            if (operator.id === "add") {
+                op = add;
+            }
+            if (operator.id === "subtract") {
+                op = subtract;
+            }
+            if (operator.id === "multiply") {
+                op = multiply;
+            }
+            if (operator.id === "divide") {
+                op = divide;
+            }
+        }
+    }));
+
+    const equal = document.querySelector("#equals");
+    equal.addEventListener("click", function() {
+        if (a && op && b) {
+            display.textContent = "";
+            show(operate(op, Number(a), Number(b)));
+            a = display.textContent;
+            b = "";
+            op = null;
+        }
+    })
+}
+
+
+setup();
