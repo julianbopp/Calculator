9355df92df0d167be5c78446b72979ec1df6589e
julianbopp <julian.bopp@tutanota.com>
2022-08-12 23:35:17 +0200

    add clear function

--- external.js
+++ external.js
@@ -1,3 +1,4 @@
+
 function add(a, b) {
     return a + b;
 }
@@ -15,7 +16,7 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
-    return op(a,b);
+    return op(a, b);
 }
 
 function show(number) {
@@ -28,60 +29,67 @@ function getDisplayValue() {
     return Number(display.textContent);
 }
 
-
-function setup() {
-    const display = document.querySelector(".display");
-    var a = "";
-    var b = "";
-    var op = null;
-    var comma = false;
-
-    const digits = document.querySelectorAll(".digit");
-    digits.forEach(digit => digit.addEventListener("click", function() {
-        if (digit.id === "comma" && comma) {
-            return;
-        }
-        if (digit.id === "comma" && !comma) {
-            comma = true;
-        }
-        show(digit.textContent);
-        if (!op) {
-            a = a + digit.textContent;
-        } else {
-            b = b + digit.textContent;
-        }
-    }));
-
-    const operators = document.querySelectorAll(".operator");
-    operators.forEach(operator => operator.addEventListener("click", function() {
-        if(!op) {
-            show(operator.textContent);
-            if (operator.id === "add") {
-                op = add;
-            }
-            if (operator.id === "subtract") {
-                op = subtract;
-            }
-            if (operator.id === "multiply") {
-                op = multiply;
-            }
-            if (operator.id === "divide") {
-                op = divide;
-            }
-        }
-    }));
-
-    const equal = document.querySelector("#equals");
-    equal.addEventListener("click", function() {
-        if (a && op && b) {
-            display.textContent = "";
-            show(operate(op, Number(a), Number(b)));
-            a = display.textContent;
-            b = "";
-            op = null;
-        }
-    })
+function clearAll() {
+    a = "";
+    b = "";
+    op = null;
+    display.textContent = "";
 }
 
 
-setup();
+const display = document.querySelector(".display");
+var a = "";
+var b = "";
+var op = null;
+var comma = false;
+
+const digits = document.querySelectorAll(".digit");
+digits.forEach(digit => digit.addEventListener("click", function () {
+    if (digit.id === "comma" && comma) {
+        return;
+    }
+    if (digit.id === "comma" && !comma) {
+        comma = true;
+    }
+    show(digit.textContent);
+    if (!op) {
+        a = a + digit.textContent;
+    } else {
+        b = b + digit.textContent;
+    }
+}));
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => operator.addEventListener("click", function () {
+    if (!op) {
+        show(operator.textContent);
+        if (operator.id === "add") {
+            console.log("test");
+            op = add;
+        }
+        if (operator.id === "subtract") {
+            op = subtract;
+        }
+        if (operator.id === "multiply") {
+            op = multiply;
+        }
+        if (operator.id === "divide") {
+            op = divide;
+        }
+    }
+}));
+
+const equal = document.querySelector("#equals");
+equal.addEventListener("click", function () {
+    if (a && op && b) {
+        display.textContent = "";
+        show(operate(op, Number(a), Number(b)));
+        a = display.textContent;
+        b = "";
+        op = null;
+    }
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearAll);
+
